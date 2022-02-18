97f0586cfbbbd74e96b8fc3daaf994c89a500d38
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2022-02-18 21:16:49 +0300

    login register ve admin panel template giydirme tamamlandı

---
+++ AdminUserAuthSystem/resources/views/admin/_topmenu.blade.php
@@ -0,0 +1,24 @@
+<div class="hidden xl:block px-6 lg:px-12 py-6 bg-blueGray-100">
+    <ul class="flex items-center"><li class="flex items-center">
+        <a class="text-sm text-darkBlueGray-300 hover:text-darkBlueGray-400 font-heading font-medium" href="#">Başvurular</a>
+        <span class="inline-block mx-4">
+          <svg width="4" height="4" viewbox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="2" cy="2" r="2" fill="#C8CDD3"></circle></svg></span>
+      </li>
+      <li class="flex items-center">
+        <a class="text-sm text-darkBlueGray-300 hover:text-darkBlueGray-400 font-heading font-medium" href="#">Personeller</a>
+        <span class="inline-block mx-4">
+          <svg width="4" height="4" viewbox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="2" cy="2" r="2" fill="#C8CDD3"></circle></svg></span>
+      </li>
+      <li class="flex items-center">
+        <a class="text-sm text-darkBlueGray-300 hover:text-darkBlueGray-400 font-heading font-medium" href="#">Evrak İncele</a>
+        <span class="inline-block mx-4">
+          <svg width="4" height="4" viewbox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="2" cy="2" r="2" fill="#C8CDD3"></circle></svg></span>
+      </li>
+      <li class="flex items-center">
+        <a class="text-sm text-darkBlueGray-300 hover:text-darkBlueGray-400 font-heading font-medium" href="#">Canlı Destek</a>
+        <span class="inline-block mx-4">
+          <svg width="4" height="4" viewbox="0 0 4 4" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="2" cy="2" r="2" fill="#C8CDD3"></circle></svg></span>
+      </li>
+      <li class="flex items-center"><a class="text-sm text-darkBlueGray-300 hover:text-darkBlueGray-400 font-heading font-medium" href="#">Ticket</a></li>
+    </ul>
+</div>
