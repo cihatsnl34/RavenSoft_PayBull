66f5937a2d4cd255ebff3e3283081ea766ed2b52
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2022-02-20 15:37:47 +0300

    refactory

--- AdminUserAuthSystem/resources/views/admin/about/about_edit.blade.php
+++ AdminUserAuthSystem/resources/views/admin/about/about_edit.blade.php
@@ -9,7 +9,7 @@
 
       <input class="w-full mb-6 py-5 px-12 text-xl border-2 border-blue-400 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" id="title" value="{{$AboutData->title}}" name="title" type="text">
       <input class="w-full mb-6 py-5 px-12 text-xl border-2 border-blue-400 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" type="text"value="{{$AboutData->description}}" name="description" id="description">
-      <button type="submit" class="block py-5 px-10 mx-auto w-full md:max-w-max text-xl leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl">Add About</button>
+      <button type="submit" class="block py-5 px-10 mx-auto w-full md:max-w-max text-xl leading-6 text-white font-medium tracking-tighter font-heading text-center bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl">Update About</button>
     </div>
   </div>
 </form>
